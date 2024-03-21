7d33b6875c9bb11ee9eecb250a144f048f716a4c
Felipe <76501827+F3LIP3X@users.noreply.github.com>
2024-03-21 13:04:46 +0100

    1.2.0
    Important changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Page1 from './components/page1/pagejsx';
-import Page2 from './components/page2/page2jsx';
+import inicio from './components/Inicio/Inicio';
+import servicios from './components/Servicios/Servicios';
+import actividades from './components/Actividades/Actividades';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route exact path="/" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
+          <Route exact path="/Inicio" element={<inicio />} />
+          <Route exact path="/Servicios" element={<servicios />} />
+          <Route exact path="/Actividades" element={<actividades />} />
         </Routes>
       </div>
     </Router>
