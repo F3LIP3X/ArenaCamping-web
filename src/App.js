4084335736350d686e34eabc16f52919ec3f7131
Ruben0-4 <127145096+Ruben0-4@users.noreply.github.com>
2024-03-21 11:08:36 +0100

    1.1.90
    Page2 (error no se ve nada)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import PageJsx from './components/page1/pagejsx.jsx';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Page1 from './components/page1/pagejsx';
+import Page2 from './components/page2/page2jsx';
 
 function App() {
   return (
-    <div className="App">
-      <PageJsx />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
