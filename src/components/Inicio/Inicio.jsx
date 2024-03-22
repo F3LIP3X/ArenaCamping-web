28cd53c8b0d98423ff56f306ea6830ee663c2064
Felipe <76501827+F3LIP3X@users.noreply.github.com>
2024-03-22 11:24:16 +0100

    1.2.4
    Logo

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -34,36 +34,6 @@ const firstIndex = () => {
                         <p>Somos un camping familiar ubicado en medio de exuberantes bosques y hermosos paisajes naturales. Ofrecemos una amplia gama de servicios para hacer que tu estadía sea cómoda y memorable.</p>
                     </section>
 
-                    <section id="prices">
-                        <h2>Tabla de Precios</h2>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Tipo de Alojamiento</th>
-                                    <th>Capacidad</th>
-                                    <th>Precio por Noche</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Camping Básico (Tienda de Campaña)</td>
-                                    <td>2 personas</td>
-                                    <td>$20</td>
-                                </tr>
-                                <tr>
-                                    <td>Bungalow Pequeño</td>
-                                    <td>4 personas</td>
-                                    <td>$50</td>
-                                </tr>
-                                <tr>
-                                    <td>Bungalow Grande</td>
-                                    <td>6 personas</td>
-                                    <td>$80</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </section>
-
                     <section id="contact">
                         <h2>Contacto</h2>
                         <p>Para realizar reservas o para obtener más información, no dudes en contactarnos:</p>
@@ -75,12 +45,14 @@ const firstIndex = () => {
 
             <footer className="footer-page">
                 <div className='foot'>
-                    <p>© 2024 | Felipe Toledano Escudero</p>
+                    <h3>© 2024 | Felipe Toledano Escudero</h3>
+                    <h3>Télefono de Contacto: +34 625 86 46 17</h3>
                     <nav>
                         <ul>
-                            <li><a href="#home">Inicio</a></li>
-                            <li><a href="#about">Acerca de</a></li>
-                            <li><a href="#contact">Contacto</a></li>
+                            <li><a>Inicio</a></li>
+                            <li><a>Acerca de</a></li>
+                            <li><a>Contacto</a></li>
+                            p     
                         </ul>
                     </nav>
                 </div>
