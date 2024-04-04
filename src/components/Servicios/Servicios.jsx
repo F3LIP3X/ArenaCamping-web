513f99834bba988894a9cba65e595ef62db3e149
Ruben0-4 <127145096+Ruben0-4@users.noreply.github.com>
2024-04-04 12:50:08 +0200

    1.4.3
    Galería de imágenes y comentado el css de servicios

--- src/components/Servicios/Servicios.jsx
+++ src/components/Servicios/Servicios.jsx
@@ -4,6 +4,15 @@ import { Link } from 'react-router-dom';
 //css
 import '../Servicios/servicios.css';
 
+//imagenes
+import arealibre from '../assets/Imagenes Servicios/arealibre.png';
+import camping from '../assets/Imagenes Servicios/camping.png';
+import duchas from '../assets/Imagenes Servicios/duchas.png';
+import electricidad from '../assets/Imagenes Servicios/electricidad.png';
+import electrodomesticos from '../assets/Imagenes Servicios/electrodomesticos.png';
+import parking from '../assets/Imagenes Servicios/parking.png';
+import vaciadoaguas from '../assets/Imagenes Servicios/vaciadoaguas.png';
+
 //icons
 import icon from '../assets/logo.ico';
 import { GiPadlock } from "react-icons/gi";
@@ -106,9 +115,32 @@ const secondIndex = () => {
                         </div>
                     </section>
 
-                    <section id="galeria">
+                    <section>
                         <h2 className='fotosg'>Galería</h2>
                         <p className='fotosg'>Descubre nuestras instalaciones</p>
+                            <div className='conjuntofotos'>
+                                <div className='box'>
+                                    <img src={arealibre} alt="img1" />
+                                </div>
+                                <div className='box'>
+                                    <img src={electricidad} alt="img2" />
+                                </div>
+                                <div className='box'>
+                                    <img src={duchas} alt="img3" />
+                                </div>
+                                <div className='box'>
+                                    <img src={camping} alt="img4" />
+                                </div>
+                                <div className='box'>
+                                    <img src={electrodomesticos} alt="img5" />
+                                </div>
+                                <div className='box'>
+                                    <img src={parking} alt="img6" />
+                                </div>
+                                <div className='box'>
+                                    <img src={vaciadoaguas} alt="img7" />
+                                </div>
+                            </div>
                     </section>
                     <section className='redes'>
                         <div id='titleRedes'>
