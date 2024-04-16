f0bb9e5a5288103d6886d1409f741c8678ce85e1
Pedro123v <145437474+Pedro123v@users.noreply.github.com>
2024-04-16 14:18:55 +0200

    1.9.5

--- src/components/Localización/Localizacion.jsx
+++ src/components/Localización/Localizacion.jsx
@@ -21,14 +21,14 @@ const locationPanel = () => {
                         <hr />
                         <p>Bem-vindo ao Camping Vale Verde, um refúgio natural que oferece uma escapada única para os amantes da natureza e da aventura. Situado em meio a exuberantes bosques e banhado pela luz do sol filtrada pelas folhas, este local de camping é um verdadeiro paraíso para quem busca desconexão e renovação.
 
-Localizado ao calhas, longe das distrações da vida cotidiana, o Camping Vale Verde oferece uma atmosfera de paz e tranquilidade que é difícil de encontrar em outros lugares. Aqui, você pode se reconectar com a natureza, respirar ar puro e recarregar suas energias enquanto desfruta do conforto simples da vida ao ar livre.
+                            Localizado ao calhas, longe das distrações da vida cotidiana, o Camping Vale Verde oferece uma atmosfera de paz e tranquilidade que é difícil de encontrar em outros lugares. Aqui, você pode se reconectar com a natureza, respirar ar puro e recarregar suas energias enquanto desfruta do conforto simples da vida ao ar livre.
 
-As opções de acomodação no Camping Vale Verde são tão variadas quanto as estrelas no céu noturno. Desde acampamentos em barracas sob as copas das árvores até áreas para trailers e motorhomes, há uma opção para cada estilo de viajante. E se você deseja uma experiência ainda mais próxima da natureza, pode até mesmo optar por acampar em um local selvagem e desfrutar da verdadeira sensação de liberdade.
+                            As opções de acomodação no Camping Vale Verde são tão variadas quanto as estrelas no céu noturno. Desde acampamentos em barracas sob as copas das árvores até áreas para trailers e motorhomes, há uma opção para cada estilo de viajante. E se você deseja uma experiência ainda mais próxima da natureza, pode até mesmo optar por acampar em um local selvagem e desfrutar da verdadeira sensação de liberdade.
 
-As atividades ao ar livre são o destaque desta experiência de camping. Explore trilhas que serpenteiam por entre as árvores centenárias, descubra riachos cristalinos onde é possível se refrescar em dias quentes e desfrute de passeios de bicicleta ou caminhadas para explorar os arredores. À noite, reúna-se ao redor de uma fogueira crepitante para contar histórias, ass
+                            As atividades ao ar livre são o destaque desta experiência de camping. Explore trilhas que serpenteiam por entre as árvores centenárias, descubra riachos cristalinos onde é possível se refrescar em dias quentes e desfrute de passeios de bicicleta ou caminhadas para explorar os arredores. À noite, reúna-se ao redor de uma fogueira crepitante para contar histórias, ass
 
 
-</p>
+                        </p>
                     </section>
 
                     <div class="container-form">
@@ -55,7 +55,7 @@ As atividades ao ar livre são o destaque desta experiência de camping. Explore
                         <p>Teléfono: 123-456-789</p>
                         <p>Email: info@campingnaturaleza.com</p>
                     </section>
-                  
+
                     <SocialPanel />
                     <FooterPanel />
                 </main>
