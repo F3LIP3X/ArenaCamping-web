ca484c17d53287e7e99aae009e24df02841af319
Felipe <76501827+F3LIP3X@users.noreply.github.com>
2024-04-17 15:41:58 +0200

    2.0.0

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -8,7 +8,7 @@ import SocialPanel from '../Moduls/social';
 import HeaderPanel from '../Moduls/header';
 
 // Icono
-import icon from '../assets/logo.ico';
+
 
 
 
@@ -21,23 +21,27 @@ const FifthIndex = () => {
                     <section>
                         <h2>Contacto</h2>
                     </section>
-                    <SocialPanel />
+
                     <section className="formulario">
                         <h2>Si tiene alguna duda, envíanos un mensaje</h2>
                         <form>
+
                             <div className="form-group">
-                                <label htmlFor="matricula" >Matrícula:</label>
+                                <label htmlFor="matricula">Matrícula:</label>
                                 <input type="text" id="matricula" name="matricula"  maxLength="7"/>
                                 <label htmlFor="email">Correo Electrónico:</label>
                                 <input type="email" id="email" name="email" maxLength="30" />
                             </div>
+
                             <div className="form-group">
                                 <label htmlFor="mensaje">Mensaje:</label>
                                 <textarea id="mensaje" name="mensaje" maxLength="200 "></textarea>
                             </div>
+
                             <button type="submit">Enviar</button>
                         </form>
                     </section>
+                    <SocialPanel />
                     <FooterPanel />
                 </main>
             </div>
