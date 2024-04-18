655e428b39396f94ed133be420e7142c4d83cd62
Vocra83 <164329519+Vocra83@users.noreply.github.com>
2024-04-18 16:03:35 +0200

    2.1.0

--- src/components/Entorno/Entorno.jsx
+++ src/components/Entorno/Entorno.jsx
@@ -9,6 +9,11 @@ import GaleryPanel from '../Moduls/galery';
 
 //Comando para comentar Ctrl + K + C
 const Entorno = () => {
+    const ButtonGastronomia = () => {
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+    }
+
+    // https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
     return (
         <div className='wrapper'>
             <HeaderPanel />
@@ -17,10 +22,10 @@ const Entorno = () => {
                     <section>
                         <h2>Inicio</h2>
                     </section>
-                    <section id="about">
-                        <h2 id='aboutUs'>Sobre Nosotros</h2>
+                    <section id="InformacionEntorno">
+                        <h2 id='aboutUs'>Información de la zona</h2>
                         <hr />
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius 
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius
                             quaerat vitae itaque blanditiis. Impedit voluptatem illum consequuntur
                             rem provident eveniet optio porro! Accusantium, distinctio! Et molestiae
                             velit natus maiores accusamus dignissimos, corrupti odit sequi porro
@@ -33,13 +38,56 @@ const Entorno = () => {
                             itaque sequi molestias harum perferendis debitis inventore
                             consectetur. Quod architecto corrupti expedita, facere est
                             cupiditate nobis tempora quis similique et dicta maxime
-                            corporis incidunt atque iusto, quam in laboriosam sed
-                            commodi pariatur. Necessitatibus commodi incidunt fuga,
-                            optio assumenda iure. Ratione nemo, accusamus ea recusandae
-                            sapiente voluptatum tenetur numquam? Nemo aspernatur
-                            aperiam itaque in optio hic aut perferendis eum similique
-                            tenetur exercitationem assumenda libero ab, molestias tempora magnam
-                            doloribus at mollitia beatae rem minima aliquid molestiae facere.</p>
+                        </p>
+                    </section>
+                    <section id="HistoriaEntorno">
+                        <h2 id='aboutUs'>Historia</h2>
+                        <hr />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius
+                            quaerat vitae itaque blanditiis. Impedit voluptatem illum consequuntur
+                            rem provident eveniet optio porro! Accusantium, distinctio! Et molestiae
+                            velit natus maiores accusamus dignissimos, corrupti odit sequi porro
+                            soluta labore fuga praesentium quo, nostrum, reprehenderit ab facere
+                            commodi deleniti ea quidem consequatur quia quos aperiam? Fuga
+                            placeat labore iusto nisi non unde cupiditate enim voluptatem
+                            debitis id ab sequi vitae, aut recusandae veniam? Excepturi
+                            tempore at adipisci quibusdam, est, ullam saepe ratione eum
+                            non cumque rerum corporis iusto reprehenderit neque id deserunt
+                            itaque sequi molestias harum perferendis debitis inventore
+                            consectetur. Quod architecto corrupti expedita, facere est
+                            cupiditate nobis tempora quis similique et dicta maxime
+                        </p>
+                    </section>
+                    <section id="GastronomiaEntorno">
+                        <h2 id='aboutUs'>Gastronomia</h2>
+                        <hr />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius
+                            quaerat vitae itaque blanditiis. Impedit voluptatem illum consequuntur
+                            rem provident eveniet optio porro! Accusantium, distinctio! Et molestiae
+                            velit natus maiores accusamus dignissimos, corrupti odit sequi porro
+                            soluta labore fuga praesentium quo, nostrum, reprehenderit ab facere
+                            commodi deleniti ea quidem consequatur quia quos aperiam? Fuga
+                            placeat labore iusto nisi non unde cupiditate enim voluptatem
+                            debitis id ab sequi vitae, aut recusandae veniam? Excepturi
+                            tempore at adipisci quibusdam, est, ullam saepe ratione eum
+                            non cumque rerum corporis iusto reprehenderit neque id deserunt
+                            itaque sequi molestias harum perferendis debitis inventore
+                            consectetur. Quod architecto corrupti expedita, facere est
+                            cupiditate nobis tempora quis similique et dicta maxime
+                        </p>
+                    </section>
+                    <section id="DondeComer">
+                        <h2 id='aboutUs'>Donde Comer</h2>
+                        <hr />
+                        <ul id='RestaurantesEntorno'>
+                            <li>Restaurante Envero</li>
+                            <li>Restaurante ocina Salvaje</li>
+                            <li>Restaurante la Pradera</li>
+                        </ul>
+                        <br />
+                        <button class="BotonEntonros" onClick={ButtonGastronomia}>¡¡Pulsa Aqui!!</button>
+
+
                     </section>
 
                     <section id="contact">
