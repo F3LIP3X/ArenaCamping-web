f77bc193303aa6098773ec6590d6afa7dcabc511
Felipe <76501827+F3LIP3X@users.noreply.github.com>
2024-04-20 04:27:38 +0200

    2.2.0

--- src/components/Entorno/Entorno.jsx
+++ src/components/Entorno/Entorno.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Entorno/Entorno.css';
-import { Link } from 'react-router-dom';
+
 
 import FooterPanel from '../Moduls/footer';
 import HeaderPanel from '../Moduls/header';
