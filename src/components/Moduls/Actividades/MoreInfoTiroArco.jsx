70b1379766044f48621a9dcdb10d8e1018f7f60e
Pedro123v <145437474+Pedro123v@users.noreply.github.com>
2024-04-23 13:25:58 +0200

    2.6.7

--- src/components/Moduls/Actividades/MoreInfoTiroArco.jsx
+++ src/components/Moduls/Actividades/MoreInfoTiroArco.jsx
@@ -5,75 +5,40 @@ import "slick-carousel/slick/slick-theme.css";
 import '../../Inicio/inicio.css';
 import '../../Actividades/actividades.css';
 
+// Imagens Tiro con Arco
+import TiroConArco1 from '../../assets/ImagenesActividades/TiroConArco1.jpg';
+import TiroConArco2 from '../../assets/ImagenesActividades/TiroConArco2.jpg';
+import tiroaoarco3 from '../../assets/ImagenesActividades/tiroaoarco3.jpg';
+import tiroaoarco4 from '../../assets/ImagenesActividades/tiroaoarco4.jpg';
+
 const MoreInfoTiroArco = () => {
-    // Função para verificar se é um dispositivo móvel
-    const isMobileDevice = () => {
-        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    };
-
-    // Se não for um dispositivo móvel, retorna null para não renderizar o componente
-    if (!isMobileDevice()) {
-        return null;
-    }
-
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3, // Define o número de slides visíveis em telas grandes
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 768, // Tamanho de tela a partir do qual o número de slides é ajustado
-                settings: {
-                    slidesToShow: 2 // Define o número de slides visíveis em telas menores
-                }
-            },
-            {
-                breakpoint: 480, // Tamanho de tela a partir do qual o número de slides é ajustado
-                settings: {
-                    slidesToShow: 1 // Define o número de slides visíveis em telas muito pequenas
-                }
-            }
-        ]
-    };
-
     return (
-        <div>
-            <section>
-                <h2>Tiro Con Arco</h2>
-                <h4>Pon a prueba tu punteria en nuestra zona de tiro con arco</h4>
-                <br />
+        <section className="responsive-gallery">
+            <h2>Tiro Con Arco</h2>
+            <h4>Pon a prueba tu punteria en nuestra zona de tiro con arco</h4>
+            <br />
 
-                {/* Carousel */}
-                <Slider {...settings}>
-                    <div>
-                        <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box" alt="Foto 1" />
+            <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
+                <Slider>
+                    <div className="carousel-item">
+                        <img src={TiroConArco1} className="rounded-box" alt="TiroConArco1" />
                     </div> 
-                    <div>
-                        <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="rounded-box" alt="Foto 2" />
+                    <div className="carousel-item">
+                        <img src={TiroConArco2} className="rounded-box" alt="TiroConArco2" />
                     </div> 
-                    <div>
-                        <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" className="rounded-box" alt="Foto 3" />
+                    <div className="carousel-item">
+                        <img src={tiroaoarco3} className="rounded-box" alt="tiroaoarco3" />
                     </div> 
-                    <div>
-                        <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" className="rounded-box" alt="Foto 4" />
-                    </div> 
-                    <div>
-                        <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" className="rounded-box" alt="Foto 5" />
-                    </div> 
-                    <div>
-                        <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" className="rounded-box" alt="Foto 6" />
-                    </div> 
-                    <div>
-                        <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" className="rounded-box" alt="Foto 7" />
+                    <div className="carousel-item">
+                        <img src={tiroaoarco4} className="rounded-box" alt="tiroaoarco4" />
                     </div>
                 </Slider>
-                
-            </section>
-        </div>
+            </div>
+        </section>
     );
 };
 
 export default MoreInfoTiroArco;
+
+
 
